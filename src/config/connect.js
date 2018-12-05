0cfb36d48192c450921061ed97dcd733cf9e5b9e
donprolad <37209574+donprolad@users.noreply.github.com>
2018-12-05 03:43:34 +0200

    api_config
    get key from requesting api at meetups.com

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,6 +1,6 @@
 const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/";
 const CATEGORIES_API_URL = "https://api.meetup.com/2/categories";
-const API_KEY = "?key=177b565f235f712f7574c6cd29550";
+const API_KEY = "?key=";
 const SIGN = "&sign=true";
 
 const SEARCH_URL = CORS_ANYWHERE + CATEGORIES_API_URL + API_KEY + SIGN;
